d85b87f9c836b31ed9566be381522a08c02c27d5
S <scnroy@users.noreply.github.com>
2022-08-02 10:01:18 -0400

    add some content

---
+++ src/content.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Press release: July 20 Press Conference Will Address Illegal Closure of Dekalb County Park',
+        image: 'https://i0.wp.com/defendtheatlantaforest.org/wp-content/uploads/2022/07/pedestrian-private-property-1.jpeg',
+        body: 'July 20 Press Conference Will Address Illegal Closure of Dekalb County Park A press conference will be held at the South River Trail Intrenchment Creek Park trailhead on West Side Place on Wednesday, July 20, at 4 pm. The conference is being organized by Dekalb community residents in response to the placement of concrete barriers […]',
+        date: '2022-07-20T04:00:00.000Z'
+    },
+    {
+        title: 'Press release: As public opposition grows, Atlanta police attempt to begin work on Cop City, protestors occupy trees',
+        image: 'https://i0.wp.com/defendtheatlantaforest.org/wp-content/uploads/2022/06/TREESIT.jpg',
+        body: 'As of June 1, Atlanta Police have accompanied contractors Long Engineering, Brasfield & Gorrie and other contracted workers to the woods to attempt to stay on schedule with a forest destruction timeline which sets clearing the forest to begin by the end of June. Activists on the ground have reported heavy machinery clearing trees along the perimeter of the proposed site, as well as seemingly random tree-felling deeper inside the forest. This work is being conducted without the proper permits from Dekalb County Office of Planning & Sustainability.',
+        date: '2022-07-01T04:00:00.000Z'
+    },
+    {
+        title: 'Emory doctors condemn Cop City, call to fund public health initiatives instead',
+        image: 'https://i0.wp.com/defendtheatlantaforest.org/wp-content/uploads/2022/06/tree_of_life1336106656972-4100596355-e1657988584795.jpg',
+        body: 'As physicians who work in Atlanta and participate within the Georgia Human Rights Clinic, and with experience evaluating asylum seekers and victims of various forms of abuses, we have grave concerns with the establishment of such a facility. We witness the further militarization of police in Atlanta and throughout this country and grow increasingly alarmed. This facility will be one of if not the largest police training facilities in the United States.',
+        date: '2022-06-06T04:00:00.000Z'
+    },
+]
